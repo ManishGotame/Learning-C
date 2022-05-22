a4ee3090a3bde8395973071ec31b3605583160df
ManishGotame <manishgt194@gmail.com>
2022-05-22 19:49:24 +0545

    base code

---
+++ base.cpp
@@ -0,0 +1,36 @@
+/*
+  === a few reminders ===  
+  
+  memset(vis, 0, sizeof(vis));
+  
+  vector<tuple<int,int,int>> x;
+  x.pb({2,3,4})
+  
+  double to single --> (n,m) => (x * m) + y ==> 0,1 -> 1
+   
+  single to double -- n and m as the input 
+    int x = val / m;
+    int y = val % m;
+
+  builtin popcounts for bit manipulations 
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define pb push_back
+#define mk make_pair
+#define f first
+#define s second
+#define ll long long 
+#define ii pair<int,int> 
+#define inf 1e9
+#define endl '\n'
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  return 0; 
+}
